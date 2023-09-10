ca1103a77fb1f1cc7cdd7d0b6cbb7bdf890355e6
Your NameVladimirs77nt <vladimirs77@yandex.ru>
2023-09-10 21:11:28 +0500

    домашние задания к семинару 4 - решаются

---
+++ DZ-04/DZ-04-1.py
@@ -0,0 +1,27 @@
+# Задание №1
+# Напишите функцию для транспонирования матрицы
+
+# функция печати матрицы
+def matrix_print (matrix):
+    for row in matrix:
+        print (' '.join(map(str,row)))
+
+# функция транспонирования матрицы
+def matrix_transposition (matrix):
+    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+
+# задаем исходную матрицу 
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9],
+          [10, 11, 12]]
+
+print ()
+print (" -- исходная матрица ---")
+matrix_print (matrix)
+print ()
+
+matrix = matrix_transposition(matrix)
+print (" -- транспонированная матрица ---")
+matrix_print (matrix)
+print ()
