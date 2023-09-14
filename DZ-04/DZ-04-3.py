4d1c4a1ad4b15b73522462dcff6a02a297b78480
Your NameVladimirs77nt <vladimirs77@yandex.ru>
2023-09-14 09:23:26 +0500

    домашние задания к семинару 4 - банкомат - завершено

--- DZ-04/DZ-04-3.py
+++ DZ-04/DZ-04-3.py
@@ -14,10 +14,186 @@
 # операцией, даже ошибочной
 # ✔ Любое действие выводит сумму денег
 
-def print_menu ():
-    print (" --- МЕНЮ БАНКОМАТА ---")
-    print (" > выберите:")
-    print ("1. Пополнить счет")
-    print ("2. Снять")
-    print ("3. Выйти")
+def init ():
+    print ()
+    print ("-"*50)
+    print ("       <<< ДОБРО ПОЖАЛОВТЬ В БАНКОМАТ >>>")
+    print ("-"*50)
+    print ()
+    global cash, operation_count, operation_record, wealth_tax
+    cash = 0                # сумма счета
+    operation_count = 0     # счетчик операций
+    operation_record = []   # журнал операций
+    wealth_tax = False      # налог на богатство
     
+def view_menu ():
+    print (" --- МЕНЮ БАНКОМАТА ---")
+    print (" >>> выберите:")
+    print ("1. Пополнить счет")
+    print ("2. Снять со счета")
+    print ("3. Выйти")
+    print ("4. Показать историю (log) операци")
+    print ()
+    view_cash ()
+
+def view_cash ():
+    global cash
+    cash = round (cash, 2)
+    print (f" >> Текущий баланс: {cash}")
+    print ()
+    
+def controller ():
+    global cash, operation_count, operation_record, wealth_tax
+    view_menu ()
+
+    while True:
+        choice = input ("Выберите действие: ")
+        match choice:
+
+            # ПОПОЛНИТЬ счет
+            case "1":
+                print ()
+                print (">> выбрано '1. Пополнить счет'")
+                print ()
+                cash_in = amount_selecrion ()
+                print ()
+                if cash_in > 0:
+                    print (f" >> Счет пополнен на {cash_in}")
+                    cash += cash_in
+                    operation_record.append (f"{operation_count}. Пополнение счета на {cash_in}. Баланс {cash}")
+                    print ()
+                    operation_count += 1
+                    if operation_count%3 == 0:
+                        view_cash ()
+                        accrual_of_money_interest (3)
+                if cash_in == 0:
+                    print (f" -- Операция прервана! Счет не пополнен...")
+                    operation_record.append (" >> Операция прервана! Счет не пополнен...")
+                    print ()
+                view_menu ()
+
+            # СНЯТЬ со счета
+            case "2":
+                print ()
+                print (">> выбрано '2. Снять со счета'")
+                print ()
+                if cash < 30:
+                    print (">> суммы на счете не хватает для выполнения операции!")
+                    print ()
+                else:
+                    cash_out = amount_selecrion ()
+                    print ()
+                    if cash_in == 0:
+                        print (f" -- Операция прервана! Со счета ничего не снято...")
+                        operation_record.append (" >> Операция прервана! Со счета ничего не снято...")
+                        print ()
+                        operation_wealth_tax (10, 5000000)
+                    elif cash_out > cash:
+                        print (f" >> Введенная сумма ({cash_out}) превышает баланс счета!")
+                        operation_wealth_tax (10, 5000000)
+                    elif cash_out > 0:
+                        # percentage_for_withdrawal_of_money - процент за снятие денег = 1,5 %
+                        percentage_for_withdrawal_of_money = round (cash_out*1.5/100, 2)
+                        if percentage_for_withdrawal_of_money < 30:
+                            percentage_for_withdrawal_of_money = 30
+                        if percentage_for_withdrawal_of_money > 600:
+                            percentage_for_withdrawal_of_money = 600
+                        if (cash_out + percentage_for_withdrawal_of_money) > cash:
+                            print (f" >> Введенная сумма ({cash_out}), с учетом процента за снятие ({percentage_for_withdrawal_of_money}), превышает баланс счета!")
+                            print (f" -- Операция прервана! Со счета ничего не снято...")
+                            operation_record.append (" >> Операция прервана! Со счета ничего не снято...")
+                        else:
+                            print (f" >> Введена сумма {cash_out}")
+                            print (f" >> Процент за снятие: {percentage_for_withdrawal_of_money}.  Итого сумма снятия: {cash_out + percentage_for_withdrawal_of_money}")
+                            cash -= (cash_out + percentage_for_withdrawal_of_money)
+                            print (f" >> Со счета снято {cash_out + percentage_for_withdrawal_of_money}")
+                        operation_record.append (f"{operation_count}. Снятие со счета суммы {cash_out + percentage_for_withdrawal_of_money}. Баланс {cash}")
+                        print ()
+                        operation_count += 1
+                        if operation_count%3 == 0:
+                            view_cash ()
+                            accrual_of_money_interest (3)
+                view_menu ()
+
+            # ВЫХОД из программы
+            case "3" | "0":
+                print ()
+                print (">> выбрано '3. Выйти'")
+                print ()
+                view_cash ()
+                print (">> Программа завершена...")
+                print ()
+                break
+
+            # ПОКАЗАТЬ ЖУРНАЛ ОПЕРАЦИЙ СО СЧЕТОМ
+            case "4":
+                print ()
+                view_cash ()
+                print (">> выбрано '4. Показать историю (log) операций'")
+                print ()
+                for i in operation_record:
+                    print(f" >> record:  {i}")
+                print (" >> record:      ----- конец записи -----")
+                print ()
+                operation_wealth_tax (10, 5000000)
+                view_menu ()
+
+            case _:
+                print ()
+                print (">> команда не распознана! пожалуйста повторите выбор...")
+                print ()
+                view_cash ()
+                view_menu ()
+
+# ИНТЕРФЕЙС ВЫБОРА (ВВОДА) СУММЫ
+def amount_selecrion ():
+    view_cash ()
+    global cash
+    print ("Внимание !!!")
+    print (" ✔ Сумма пополнения и снятия кратны 50 у.е.")
+    print (" ✔ Процент за снятие — 1.5% от суммы снятия, но не менее 30 и не более 600 у.е.")
+
+    while True:
+        print ()
+        amount_selection = input ("Введите сумму (0 - выход): ")
+        print ()
+        if amount_selection == "0":
+            return 0
+        if amount_selection.isdigit():
+            if int(amount_selection)%50 == 0:
+                cash_input = int(amount_selection)
+                return cash_input
+            else:
+                print (" >>> Ошибка ввода суммы !!! ✔ Сумма пополнения и снятия кратны 50 у.е.")
+                print (" ✔ Процент за снятие — 1.5% от суммы снятия, но не менее 30 и не более 600 у.е.")
+        else:
+            print (" >>> Ошибка ввода !!! ✔ Введите число !")
+
+# НАЧИСЛЕНИЕ ДЕНЕЖНЫХ ПРОЦЕНТОВ      
+def accrual_of_money_interest (proc):
+    global cash
+    summ_proc = round (cash*proc/100, 2)
+    print (f" >> НАЧИСЛЕНЫ ПРОЦЕНТЫ ( +{proc}% ) !!! => {summ_proc}")
+    cash += round(summ_proc, 2)
+    operation_record.append (f" -- Начислены проценты: +{proc}% (+{summ_proc}). Баланс {cash}")
+    print ()
+
+# ОПЕРАЦИЯ СНЯТИЯ НАЛОГА НА БОГАТСТВО
+def operation_wealth_tax (tax, wealth_limit):
+    global cash, wealth_tax
+    if cash < wealth_limit:
+        wealth_tax = False
+        return False
+    wealth_tax = True
+    summ_tax = round (cash*tax/100, 2)
+    print ()
+    print (f" >> НАЧИСЛЕН НАЛОГ НА БОГАТСТВО ( -{tax}% ) !!! => -{summ_tax}")
+    cash -= summ_tax
+    operation_record.append (f" -- Начислен налог на богатство: -{tax}% (-{summ_tax}). Баланс {cash}")
+    print ()
+    view_cash ()
+    return True
+
+###########################################################
+init ()
+controller ()
