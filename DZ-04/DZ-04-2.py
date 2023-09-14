055a1b9f3d865289902959d031100f9b72ecfd2c
Your NameVladimirs77nt <vladimirs77@yandex.ru>
2023-09-14 10:54:29 +0500

    домашние задания к семинару 4 - задание 1 проверка на хэшируемость - завершено

--- DZ-04/DZ-04-2.py
+++ DZ-04/DZ-04-2.py
@@ -4,34 +4,14 @@
 # где ключ — значение переданного аргумента, а значение — имя аргумента.
 # Если ключ не хешируем, используйте его строковое представление.
 
-import re
-from ast import literal_eval
-from re import sub as re_sub
+def func(**args):
+    return {value if value.__hash__ is not None else str(value):key for key, value in args.items()}
+# for key, value in args.items() - перебираем все входящие значения
+# # value.__hash__ is not None - выражение проверки ключа на хэшируемость
+# else str(value):key        - если ключ не хэшируемый - то переводим его в строкое представление
 
-def func_dict_hash (data):
-    string = str(globals())
-    index_start = re.search(r'\b(dict_key)\b', string).end()+3
-    item_str = string[index_start:-1]
-    print ((item_str), type_from_string(item_str))
- 
-def type_from_string(string):
-    total = []
-    for i in re_sub(',( )+',',',string).split():
-        total.append(type(literal_eval(i)))
-    return total
-
-print(type_from_string('True     5 [5,  8] 1e+5'))
-
-
-
-
-data_list = [1, "текст", True, 2.32, [1, 2, 3], 23, 54, {5, 4, 3, 2, 1}, "радио"]
-
-for dict_key in data_list:
-    func_dict_hash (dict_key)
-    print ()
-
-
-
-string = 'This is laughing laugh'
-
+print (func (num = 542, txt = "текст", flag = True))
+print (func (list_data = [1, 2, 3, 4, 5]))
+print (func (dict_data = {1:"один", 2:2.0, 3:None}))
+print (func (null_var = None, empty_data = []))
+print (func (s1 = 23, s2 = 23, s3 = 23))
