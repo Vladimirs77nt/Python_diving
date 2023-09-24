e7b6fab215ba54fc1af2cfad6ee67d013aa1f2b7
Your NameVladimirs77nt <vladimirs77@yandex.ru>
2023-09-25 03:07:11 +0500

    домашние задачи к семинару 7 - решены

--- DZ_07/generator_many_files.py
+++ DZ_07/generator_many_files.py
@@ -63,4 +63,4 @@ def generate_many_file (_dir, **kwargs):
 dir_path = "/DZ_07/files"    # <-- рабочая папка с файлами
 
 # запуск генерации файлов 
-generate_many_file (dir_path, mov=2, mp4=3, avi=5, txt=5, doc=6, cdr=2, jpg=3, tiff=4, gif=2, svg=3, ai=10, twg=10, ght=5, vbn=8)
+generate_many_file (dir_path, mov=10, avi=5, txt=5, doc=5, cdr=5, jpg=5, gif=10, twg=5)
