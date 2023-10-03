9ee04fba6b3a5dbc77791528ea345f5a93110c0b
Your NameVladimirs77nt <vladimirs77@yandex.ru>
2023-10-03 22:31:58 +0500

    семинар 9 - решение задач (1, 2, 3, 4..

--- Task_09/Task_09_3.py
+++ Task_09/Task_09_3.py
@@ -1,8 +1,47 @@
 # Задание №3
 
-# Напишите декоратор, который сохраняет в json файл параметры декорируемой функции и результат, который она
-# возвращает. При повторном вызове файл должен расширяться, а не перезаписываться.
+# Напишите декоратор, который сохраняет в json файл параметры декорируемой функции и результат,
+# который она возвращает. При повторном вызове файл должен расширяться, а не перезаписываться.
 # Каждый ключевой параметр сохраните как отдельный ключ json словаря.
 # Для декорирования напишите функцию, которая может принимать как позиционные, так и ключевые аргументы.
 # Имя файла должно совпадать с именем декорируемой функции.
 
+import json
+import os
+from typing import Callable
+
+
+def decor (func: Callable):
+    def wrapper (*args, **kwargs):
+        file_name = func.__name__ + ".json"
+        with open (file_name, "r", encoding="utf-8") as file:
+            try:
+                data = json.load (file)
+            except json.JSONDecodeError:
+                data = {}
+        result = str (func (*args, **kwargs))
+        data [result] = list (args + tuple(kwargs.items()))
+        with open (file_name, "w", encoding="utf-8") as file:
+            json.dump (data, file, indent=4, ensure_ascii=False )
+        return result
+    return wrapper
+
+@decor
+def factorial (x):
+    f = 1
+    for i in range (2, x+1):
+        f *= i
+    return f
+
+
+# ----------------------------- 
+
+print (factorial (5))
+print (factorial (10))
+print (factorial (15))
+print (factorial (x=5))
+print (factorial (15))
+print (factorial (15))
+print (factorial (15))
+print (factorial (x = 10))
+print (factorial (x = 0))
--- Task_09/Task_09_4.py
+++ Task_09/Task_09_4.py
@@ -3,3 +3,37 @@
 # Создайте декоратор с параметром.
 # Параметр - целое число, количество запусков декорируемой функции
 
+import json
+import os
+from typing import Callable
+
+def decor_area (area=0):
+    area = abs (area)
+
+    def decor (func: Callable):
+
+        def wrapper (x):
+            full_result = ""
+            for i in range (x-area, x+area+1): # верхний предел факторила
+                if i < 1:
+                    continue
+                result = func (i)
+                line = ""
+                for j in range (1, i):
+                    line += str(j) + " х "
+                line += str (i) + " = " + str (result)
+                full_result += line + "\n"
+            return full_result
+        return wrapper
+    return decor
+
+@decor_area(5)
+def factorial (x):
+    f = 1
+    for i in range (2, x+1):
+        f *= i
+    return f
+
+# ----------------------------- 
+a = factorial (10)
+# print (a)
