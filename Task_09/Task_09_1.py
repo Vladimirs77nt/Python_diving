366d9d045dc02a3570cdff42ad5c5a6a97c19fcc
Your NameVladimirs77nt <vladimirs77@yandex.ru>
2023-10-03 16:44:40 +0500

    семинар 9 - решение задач (1, 2, ..

--- Task_09/Task_09_1.py
+++ Task_09/Task_09_1.py
@@ -5,3 +5,49 @@
 #  - от 1 до 10 для количества попыток
 # Функция возвращает функцию, которая через консоль просит угадать загаданное число за указанное число попыток. 
 
+from random import randint
+
+def igra ():
+
+    min = 0
+    # max = 100
+    max = int (input ("Введите число до скольки можно загадать? = "))
+    # count = 7
+    count = int (input ("Сколь попыток разрешается? = "))
+    
+    hidden_number = -1
+    if hidden_number < 0:
+            hidden_number = randint (min, max)
+
+    def func_ugadai ():
+        nonlocal min, max, hidden_number, count
+        print()
+        print (f"Загадано число от {min} до {max} - попробуйте отгадать его меньше чем за {count} попыток")
+        print()
+        while True:
+            num  = int (input(f"Введите число от {min} до {max}: "))
+            if num <min or num>max:
+                print ("Введено неверное число!")
+            elif num == hidden_number:
+                print(">>> Ты угадал число! за", count, "попыток")
+                print()
+                break
+            elif num>hidden_number:
+                print("Меньше!")
+            elif num<hidden_number:
+                print("Больше!")
+            count -= 1
+            if count <= 0:
+                print()
+                print("Ты проиграл! Попытки исчерпаны...")
+                print("... а число было загадано =", hidden_number)
+                break
+            print(f" ..осталось {count} попыток...")
+            print()
+
+    return func_ugadai
+
+primer = igra ()
+
+primer ()
+primer ()
--- Task_09/Task_09_2.py
+++ Task_09/Task_09_2.py
@@ -5,3 +5,53 @@
 # Он должен проверять входят ли переданные в функцию угадайку числа в диапазоны [1, 100] и [1, 10].
 # Если не входят, вызывать функцию со случайными числами из диапазонов.
 
+from random import randint
+from typing import Callable
+
+def decorator (game: Callable):
+    def wrapper (*args, **kwargs):
+        max, count, *_ = args
+        if not (0 < max < 101):
+            max = randint (1, 100)
+            print (f"Верхний предел загадываемого числа изменен на {max}")
+        else: print ("+")
+        if not (0 < count < 11):
+            count = randint (1, 10)
+            print (f"Количество попыток на отгадывание изменено на {count}")
+        else: print ("+")
+        return game (max, count)
+    
+    return wrapper
+
+@decorator
+def ugadai_chislo (max, count):
+    min = 0
+    hidden_number = randint (min, max)
+    count_max = count
+    print()
+    print (f"Загадано число от {min} до {max} - попробуйте отгадать его меньше чем за {count} попыток")
+    print()
+    while True:
+        num  = int (input(f"Введите число от {min} до {max}: "))
+        if num <min or num>max:
+            print ("Введено неверное число!")
+        elif num == hidden_number:
+            print(f">>> Ты угадал число! за {count_max - count + 1} попыток")
+            print()
+            return True
+        elif num>hidden_number:
+            print("Меньше!")
+        elif num<hidden_number:
+            print("Больше!")
+        count -= 1
+        if count <= 0:
+            print()
+            print("Ты проиграл! Попытки исчерпаны...")
+            print("... а число было загадано =", hidden_number)
+            return False
+        print(f" ..осталось {count} попыток...")
+        print()
+
+
+# ----- БЛОК ЗАПУСКА
+print ( ugadai_chislo (1000, 1000))
