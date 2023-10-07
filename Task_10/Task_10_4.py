7958861ae9d9ddd0c96a32f884bfa45bb98830ee
Your NameVladimirs77nt <vladimirs77@yandex.ru>
2023-10-07 21:47:52 +0500

    семинар 10 - задачи решены

--- Task_10/Task_10_4.py
+++ Task_10/Task_10_4.py
@@ -7,14 +7,16 @@
 # ○ уровень доступа вычисляемый как остаток от деления суммы цифр id на семь
 
 
-from sympy import summation
 from Task_10_3 import People
 
 class Worker (People):
-    def __init__(self, last_name, first_name, patronymic, age, w_id, w_level):
+    def __init__(self, last_name, first_name, patronymic, age, w_id):
         super().__init__(last_name, first_name, patronymic, age)
         self.w_id = w_id
-        self.w_level = sum(lambda x: int(x) in str(w_id))//7
+        self.w_level = sum(map(int, str(w_id)))%7
 
     def show_worker (self):
-        return self.last_name + " " + self.first_name + " " + self.patronymic + " " + str(id) + " " + str (w_level)
+        return self.last_name + " " + self.first_name + " " + self.patronymic + " " + str(self.w_id) + " " + str (self.w_level)
+    
+worker_1 = Worker ("Петров", "Иван", "Васильевич", 35, 123456)
+print (worker_1.show_worker ())
---
+++ Task_10/test.py
@@ -0,0 +1,5 @@
+w_id = 654325
+
+lvl = sum(map(int, str(w_id)))%7
+
+print (w_id, lvl)
