84223393b50e0b69fc7c45b564b3a4a72feac352
Your NameVladimirs77nt <vladimirs77@yandex.ru>
2023-10-28 11:19:48 +0500

    семинар 15 - решение домашней задачи В ПРОЦЕССЕ

--- DZ_15/DZ_15 copy.py
+++ DZ_15/DZ_15 copy.py
@@ -13,15 +13,18 @@
 import os
 import json
 from collections import namedtuple
+import pprint
 
 
 # функция рекурсивного обхода папок и файлов
 def info_dir_and_file (dir_path, nametuple_list=None, size=0):
 
-    if not nametuple_list:
+    if nametuple_list is None:    # ИНИЦИАЛИЗАЦИЯ ДЛЯ ОБЪЕКТОВ namedtuple
+        dir_path = dir_path.replace ("\\","/")
         dct = {"Name": "", "Extension": "", "is_Folder": False, "Parrent_folder": "", "Size": 0}
         Result_tuple = namedtuple("Result_tuple", dct)
         nametuple_list = []
+        print (f" >>> ИНИЦИАЛИЗАЦИЯ ДЛЯ ПАПКИ {dir_path} <<<\n")
 
     data_dir = os.listdir (dir_path)    # получаем список папок и файлов в обследуемой папке
     list_dir = []       # список папок
@@ -35,30 +38,39 @@ def info_dir_and_file (dir_path, nametuple_list=None, size=0):
     
     # обход папок
     for _dir in list_dir:
+        print (f" --- ОБХОД ПАПКИ {_dir} ---\n")
         nametuple_list, _size = info_dir_and_file (_dir, nametuple_list)
         size += _size
 
     # обход файлов внутри папки
-    file_list = []
+    print (f" --- ОБХОД !ВНУТРИ! ПАПКИ {dir_path} ---\n")
+    nametuple_list_file = []
     for i_file in list_file:
-        i_size = os.path.getsize(i_file)
+        i_size = os.path.getsize(i_file)    # размер файла
 
-        *a, b = i_file.split("/")               # слитуем по "\" на 2 части: всю "левую" часть до последней "\"" - и далее до конца
-        path_file = "\\".join(i for i in a)         # "левую" часть (a) сплитуем, собираем обратно - это полный путь
-        # так как названия бывают с точками - определяем где последняя точка, перед расширением
-        *file_name, file_extension = b.split(".")   # "прааую" часть (b) сплитуем по точкам на 2 части: 
-                                                    # всю "левую" часть до последней ".", и "правую" - где только расширение
+        # магия получения имения файла, полного пути до него и расширения
+        *_, file_name = i_file.split("/")               # отбрасываем левую часть до послежней "\"
+        *file_name, file_extension = file_name.split(".")   # "прааую" часть (b) сплитуем по точкам на 2 части: всю "левую" часть до последней ".", и "правую" - где только расширение
         file_name = ".".join(i for i in file_name)  # "левую" часть собираем обратно - это полное название без расширения
+       
+        if file_name == "": # обработка ситуации когда у файла нет расширения
+            file_name = file_extension
+            file_extension = "---"
 
-        dir_path = dir_path.replace ("\\","/")
-        i_file = i_file.replace ("\\","/")
+        nametuple_list_file.append (Result_tuple (file_name, file_extension, False, dir_path, i_size))
+        print (f" + добавлен файл {file_name} ({dir_path})")
 
-        file_list.append (Result_tuple (file_name, file_extension, False, dir_path, i_size))
-        size += i_size
+        size += i_size  # суммируем все файлы по размеры - это размер текущей папки
+    
+    *dir_path, dir_name = dir_path.split("/")
+    dir_path = "/".join(i for i in dir_path)
 
-    dir_path = dir_path.replace ("\\","/")
-    nametuple_list.append (Result_tuple (dir_path, None, True, dir_path, size))
-    nametuple_list += file_list
+    # в итоговый список объектов добавляем информацию о папке
+    nametuple_list.append (Result_tuple (dir_name, None, True, dir_path, size))
+    print (f" +++ добавлена папка {dir_name} ({dir_path})")
+
+    # после добавления объекта папки добавляем файлы, которые есть в этой папке
+    nametuple_list += nametuple_list_file  
 
     return nametuple_list, size
 
@@ -69,21 +81,6 @@ def write_json (_result_dict, file_name):
     # запись JSON
     with open(file_json, 'w', encoding='utf-8') as f:
         json.dump(_result_dict, f, indent=4, ensure_ascii=False)
-
-# Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
-#         - принимает на вход строку — абсолютный путь до файла.
-def func_file_path (file):
-    *a, b = file.split("\\")               # слитуем по "\" на 2 части: всю "левую" часть до последней "\"" - и далее до конца
-    path_file = "\\".join(i for i in a)         # "левую" часть (a) сплитуем, собираем обратно - это полный путь
-    # так как названия бывают с точками - определяем где последняя точка, перед расширением
-    *file_name, file_extension = b.split(".")   # "прааую" часть (b) сплитуем по точкам на 2 части: 
-                                                # всю "левую" часть до последней ".", и "правую" - где только расширение
-    file_name = ".".join(j for j in file_name)  # "левую" часть собираем обратно - это полное название без расширения
-    print ()
-    print (f"            Путь:   {path_file}")
-    print (f"       имя файла:   {file_name}")
-    print (f"расширение файла:   {file_extension}")
-    print ()
 
 
 # --------------- ЗАПУСК ПРОГРАММЫ ------------------
@@ -96,18 +93,26 @@ print ()
 
 
 base_dir_path = os.getcwd()
-dir_path = base_dir_path + "/DZ_06"    # <-- рабочая папка для работы
+dir_path = base_dir_path + ""    # <-- рабочая папка для обследования
 os.chdir(dir_path)
+print (" - Папка для обследования: ", dir_path)
 
-result, *_ = info_dir_and_file (dir_path)
+result, _ = info_dir_and_file (dir_path)
+
 print ("--РЕЗУЛЬТАТ--")
+# [0] Name
+# [1] Extension
+# [2] is_Folder True/False
+# [3] Parrent_folder
+# [4] Size
+
 for file_info in result:
-    print (file_info)
-    for key, value in file_info.items():
-        print (f"{key}:{value}")
+    if file_info[2]:
+        print (f" > Папка {file_info[0]}\n\t{file_info[3]}\n\tразмер: {file_info[4]}")
+    else:
+        print (f"  - файл: {file_info[0]}\n\t.{file_info[1]}\n\t{file_info[3]}\n\tразмер: {file_info[4]}")
 
-
-dir_path = base_dir_path + "/DZ_15"    # <-- рабочая папка для записи
+dir_path = base_dir_path + "\DZ_15"    # <-- рабочая папка для записи
 file_name = "result_scan_copy"
 os.chdir(dir_path)
 write_json (result, file_name)
